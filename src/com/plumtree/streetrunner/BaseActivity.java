1ae5e948d0a811cdbd291ae2f0aa528ed9fbb565
ArkeologeN <hamzawaqas.programmer@gmail.com>
2012-09-20 17:43:34 +0500

    Initial Deploy

---
+++ src/com/plumtree/streetrunner/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.plumtree.streetrunner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
