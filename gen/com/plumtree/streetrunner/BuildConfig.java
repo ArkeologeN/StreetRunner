a989a81c1584899296817dd915295c1c0cb729dc
UmairJameel <jameel@plumtreegroup.net>
2012-09-20 18:04:45 +0500

    Project Build Modified

---
+++ gen/com/plumtree/streetrunner/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.plumtree.streetrunner;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/plumtree/streetrunner/BaseActivity.java
+++ src/com/plumtree/streetrunner/BaseActivity.java
@@ -1,13 +1,35 @@
 package com.plumtree.streetrunner;
 
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BaseActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class BaseActivity extends BaseGameActivity {
+
+	@Override
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Engine onLoadEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
